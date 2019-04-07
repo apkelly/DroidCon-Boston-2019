35660ac3dbe88eacc7009aaa0daba53c08825b50
Andrew Kelly <andy@Andrews-MacBook-Pro.local>
2019-04-07 16:20:48 -0400

    Android app with facial detection

--- facial-recognition/android-app/app/src/main/java/com/droidcon/boston2019/facialrecognition/MLKitActivity.kt
+++ facial-recognition/android-app/app/src/main/java/com/droidcon/boston2019/facialrecognition/MLKitActivity.kt
@@ -1,8 +1,13 @@
 package com.droidcon.boston2019.facialrecognition
 
 import android.util.Log
+import com.droidcon.boston2019.facialrecognition.detect.mlkit.FaceDetector
+import com.droidcon.boston2019.facialrecognition.detect.mlkit.FaceGraphic
+import com.droidcon.boston2019.facialrecognition.detect.mlkit.FrameMetadata
 import com.droidcon.boston2019.facialrecognition.detect.mlkit.MLCameraSource
+import com.google.firebase.ml.vision.face.FirebaseVisionFace
 import java.io.IOException
+import java.nio.ByteBuffer
 
 class MLKitActivity : AbstractActivity() {
 
@@ -17,6 +22,49 @@ class MLKitActivity : AbstractActivity() {
      */
     override fun createCameraSource() {
         mCameraSource = MLCameraSource(this, mGraphicOverlay)
+        mCameraSource?.apply {
+            setFrameDetector(
+                FaceDetector(object : FaceDetector.DetectorCallback {
+                    override fun onSuccess(
+                        frameData: ByteBuffer,
+                        results: List<FirebaseVisionFace>,
+                        frameMetadata: FrameMetadata) {
+
+                        if (results.isEmpty()) {
+                            // No faces in frame, so clear frame of any previous faces.
+                            mGraphicOverlay.clear()
+                        } else {
+                            // We have faces
+                            results.forEach { face ->
+                                val existingFace = mGraphicOverlay
+                                    .find(face.trackingId) as FaceGraphic?
+
+                                if (existingFace == null) {
+                                    // A new face has been detected.
+                                    val faceGraphic = FaceGraphic(
+                                        face.trackingId,
+                                        mGraphicOverlay
+                                    )
+
+                                    mGraphicOverlay.add(faceGraphic)
+
+                                } else {
+                                    // We have an existing face, update its position.
+                                    existingFace.updateFace(face)
+                                }
+                            }
+
+                            mGraphicOverlay.postInvalidate()
+
+                        }
+                    }
+
+                    override fun onFailure(exception: Exception) {
+                        exception.printStackTrace()
+                    }
+                })
+            )
+        }
     }
 
     /**
